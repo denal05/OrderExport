d941e3e092e39d4d109d84c17c920d08a97b4cc5
Denis Aleksandrov <denis.a@ontapgroup.com>
2022-01-17 14:05:35 +0100

    Defined the class in Plugin/GetAvailableMethods.php Defined the plugin in etc/di.xml Added the 'shared' attribute in the <observer> node in etc/events.xml; By default, observers are shared.

--- Plugin/GetAvailableMethods.php
+++ Plugin/GetAvailableMethods.php
@@ -2,7 +2,46 @@
 
 namespace SwiftOtter\OrderExport\Plugin;
 
+use Magento\Payment\Model\MethodList;
+use Psr\Log\LoggerInterface;
+use Magento\Framework\Message\ManagerInterface as MessageManager;
+
 class GetAvailableMethods
 {
+    private $debug;
+    private $messageManager;
+    private $logger;
 
+    public function __construct(
+        MessageManager $mm,
+        LoggerInterface $l
+    ) {
+        $this->messageManager = $mm;
+        $this->logger = $l;
+    }
+
+    public function afterGetAvailableMethods(
+        MethodList $subject,
+        $result,
+        \Magento\Quote\Api\Data\CartInterface $quote = null
+    ) {
+        $debug = true;
+        try
+        {
+            $message = "Customer Email: " . $quote->getCustomer()->getEmail();
+
+            $this->messageManager->addNoticeMessage($message);
+
+            $zendLogWriter = new \Zend\Log\Writer\Stream(BP . '/var/log/SwiftOtter_OrderExport_Plugin_GetAvailableMethods.log');
+            $zendLogLogger = new \Zend\Log\Logger();
+            $zendLogLogger->addWriter($zendLogWriter);
+            if ($debug) { $zendLogLogger->info('------------------------------------------'); }
+            if ($debug) { $zendLogLogger->info($message); }
+
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return $result;
+    }
 }
